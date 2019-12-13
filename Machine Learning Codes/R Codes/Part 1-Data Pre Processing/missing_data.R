74c237db1408b9b81849b3bd54859ad5f506fae0
amrit2356 <amritanshu.pandey@intel.com>
2019-12-13 11:06:49 +0530

    added data.csv to coding folders

--- Machine Learning Codes/R Codes/Part 1-Data Pre Processing/missing_data.R
+++ Machine Learning Codes/R Codes/Part 1-Data Pre Processing/missing_data.R
@@ -1,7 +1,7 @@
 # Data Preprocessing
 
 # Importing the dataset
-dataset = read.csv('Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Data.csv')
+dataset = read.csv('C:/Users/pandeyam/Desktop/Machine Learning/A-Z Machine Learning Practice code/ML_Practice_Code/Machine Learning Codes/R Codes/Part 1-Data Pre Processing/Data.csv')
 
 # Taking care of missing data
 dataset$Age = ifelse(is.na(dataset$Age),
@@ -10,3 +10,5 @@ dataset$Age = ifelse(is.na(dataset$Age),
 dataset$Salary = ifelse(is.na(dataset$Salary),
                         ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
                         dataset$Salary)
+print(dataset$Age)
+print(dataset$Salary)
